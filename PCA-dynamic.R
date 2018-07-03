cf2733b2b075171a061ed25a97cdad4f45dc8756
tbrunetti <brunettt@colorado.edu>
2018-07-03 11:52:35 -0600

    executable added

--- PCA-dynamic.R
+++ PCA-dynamic.R
@@ -17,6 +17,7 @@
 
 
 # ggfortify and ggplot are required for plotting and clustering PCs to metadatas
+library(factoextra)
 library(ggfortify)
 library(ggplot2)
 library(data.table)
@@ -25,16 +26,18 @@ library(gplots)
 
 ################################################################################
 ##function: prep_data(counts_file, metadata_file, output_name)                ##
-##input:                                                                      ##
-##output:                                                                     ##
+##input: results returned from output_file in addtion to counts_file path     ##
+##       and metadata_file path                                               ##
+##output: generates PDF of PC plot results (scree plot and PC1 vs PC2) and    ##    
+##        calls function model_pcs(pca_matrix, metadata_file, output_name,    ##
+##        cat_vars, num_vars, all_vars) -- added tryCatch error handling      ##
 ################################################################################ 
-
 prep_data <- function(counts_file, metadata_file, output_name){
   print(counts_file)
   print(metadata_file)
   print(output_name)
   tryCatch({
-    pdf(file=output_name)
+    pdf(file=paste(output_name, '.pdf', sep = ''))
     varTypes <- read.csv(file=metadata_file,nrows=2,header = FALSE)
     
     #determines if variable is categorical or numerical/continuous
@@ -95,12 +98,6 @@ prep_data <- function(counts_file, metadata_file, output_name){
     print(counts_sorted)
     print(metadata_sorted)
     
-    
-    # This part is more important if you are removing data from analysis.  It has to extract and collapse missing
-    # levels from the dataframe of data that was removed
-    #print("getting levels")
-    #sapply(metadata_dataframe,levels)
-    
     # Calculation of the prinipal components using prcomp
     # the t() function means to take the transpose of the counts matrix
     # scale.=TRUE means PCs will be based on corrlation matrix and not covariance matrix
@@ -113,25 +110,24 @@ prep_data <- function(counts_file, metadata_file, output_name){
     # looking to incorporate the min number of PCs before "elbowing-effect" into the model unless
     # a PC is strongly correlated with a variable in the metadata set, in which case,
     # just regress out the variable rather than the PC
-    plot(pca_matrix, type ="l")
+    screeplot(pca_matrix, type ="l")
+    print(fviz_eig(pca_matrix))
+    
     getMetadata_headers = colnames(metadata_sorted)
     print(pca_matrix$x)
     
     for(var in getMetadata_headers){
       print(var)
       if(var %in% cat_vars){
-        print(metadata_sorted)
         metadata_sorted[,var] <- (as.factor(metadata_sorted[,var]))
-        pca_plot <- autoplot(prcomp(t(counts_sorted)), data = metadata_sorted, colour = var)
-        ggsave(file=paste('temp_', var, '.png', sep=""), h=6, w=6, units="in",dpi=300)
+        print(autoplot(prcomp(t(counts_sorted)), data = metadata_sorted, colour = var))
       }else if(var %in% num_vars){
-        pca_plot <- autoplot(prcomp(t(counts_sorted)), data = metadata_sorted, colour = var) + scale_color_gradientn(colours = terrain.colors(7))
-        ggsave(file=paste('temp_', var, '.png', sep=""), h=6, w=6, units="in",dpi=300)
+        print(autoplot(prcomp(t(counts_sorted)), data = metadata_sorted, colour = var) + scale_color_gradientn(colours = terrain.colors(7)))
       }
     }
     
-    write.csv(pca_matrix$x, file="pca_matrix_output.csv")
-    write.csv(metadata_sorted, file="metadata_sorted_output.csv")
+    write.csv(pca_matrix$x, file=paste(output_name, "_pca_matrix_output.csv", sep = ''))
+    write.csv(metadata_sorted, file=paste(output_name, "_metadata_sorted_output.csv", sep=''))
     return(model_pcs(pca_matrix=pca_matrix, metadata_file=metadata_sorted, output_name=output_name, cat_vars = cat_vars, num_vars = num_vars, all_vars=getMetadata_headers))}
     ,
     error=function(error_message){
@@ -146,9 +142,12 @@ prep_data <- function(counts_file, metadata_file, output_name){
 
 # ***************************Function to correlate PCs with metadata******************************
 ################################################################################
-##function: model_pcs()                ##
-##input:                                                                      ##
-##output:                                                                     ##
+##function: model_pcs(pca_matrix, metadata_file, output_name, cat_vars,       ##
+##          num_vars, all_vars)                                               ##
+##input: output from prep_data(counts_file, metadata_file, output_name)       ##
+##output: PDF of heatmap with linear models for every variable listed in the  ##
+##        metadata up to the first 10 PCs annotated with -log10(pvals) and    ##
+##        adjusted-Rsq values -- added tryCatch error handling                ##
 ################################################################################ 
 model_pcs <- function(pca_matrix, metadata_file, output_name, cat_vars, num_vars, all_vars){
   tryCatch({
@@ -159,9 +158,16 @@ model_pcs <- function(pca_matrix, metadata_file, output_name, cat_vars, num_vars
     adjRsq <- data.frame()
     # iterate through all PCs and get -log10(p-value) and adjusted R-squared value to every PC correlated to each
     # metadata column listed
+    
+    # pick whichever value is smallest (10 PCs or total variables in metadata)
+    total_pcs = 10
+    if(length(all_vars)<10){
+      total_pcs = length(all_vars)
+    }
+    
     for (pc in seq(1,dim(pca_matrix$rotation)[2])){
       print(pc)
-      for (variable in seq(1,length(all_vars))){
+      for (variable in seq(1,total_pcs)){
         print(all_vars[variable])
         if(all_vars[variable] %in% cat_vars){
           linear_model <- lm(pca_matrix$x[,pc] ~ na.omit(as.factor(metadata_file[, all_vars[variable]])))
@@ -183,13 +189,12 @@ model_pcs <- function(pca_matrix, metadata_file, output_name, cat_vars, num_vars
     #print(factors_affecting_pcs)
     rownames(pvalues) <- all_vars
     print(pvalues)
-    colnames(pvalues) <- unlist(lapply(seq(1,length(all_vars)),function(x) paste(c('PC',x),collapse='')))
+    colnames(pvalues) <- unlist(lapply(seq(1,total_pcs),function(x) paste(c('PC',x),collapse='')))
     print(pvalues)
-    
     
     # get the row and column names matching the adj R-sq values
     rownames(adjRsq) <- all_vars
-    colnames(adjRsq) <- unlist(lapply(seq(1,length(all_vars)),function(x) paste(c('PC',x),collapse='')))
+    colnames(adjRsq) <- unlist(lapply(seq(1,total_pcs),function(x) paste(c('PC',x),collapse='')))
     
     # round all -log10(pvalue) in the dataframe to three decimal places
     is.num <- sapply(pvalues, is.numeric)
@@ -211,8 +216,6 @@ model_pcs <- function(pca_matrix, metadata_file, output_name, cat_vars, num_vars
 }
 
 #*********************************************End of function*********************************************
-
-#model_pcs(pca_matrix)
 
 #---------------------------regress out variables of interest-------------------------------------------------
 # for each variable that is to be regressed a linear model must be made and the residuals of the linear model
@@ -252,17 +255,11 @@ model_pcs <- function(pca_matrix, metadata_file, output_name, cat_vars, num_vars
 
 
 
-
-
 ################################################################################
 ##function: get_user_input()                                                  ##
-##input:  None -- prompts user for location of counts_file and metadata_file  ##
-##        (properly formatted)                                                ##
-##output: If count_file path is verified as correct, proceeds to function     ##  
-##        get_metadata(); will prompt user for path to metadata file; if      ## 
-##        verified as correct, proceeds to PCA; else at any stage it is not   ##
-##        verified then prompts user to enter path again (recursive loop      ##
-##        until path is verified) unless user quits program                   ##
+##input:  None -- prompts user for location of counts_file                    ##
+##output: Takes user input and proceeds to function                           ##
+##        val_user_input(verification_1)                                      ##
 ################################################################################ 
 get_user_input <- function(){
   cat("Full path to counts file: ")
@@ -271,32 +268,64 @@ get_user_input <- function(){
   cat('\n')
   cat('Is this correct? (Y/N/Q)')
   verification_1 <- trimws(readLines('stdin', n=1), which="both")
+  return(val_user_input(counts_file = count_file, verification_1=verification_1))
+}
+  
+################################################################################
+##function: val_user_input(verification_1)                                    ##
+##input:  verification value returned from function get_user_input()          ##
+##output: If count_file path is verified as correct, proceeds to function     ##
+##        output <- get_metadata(); if user specifies no or n then recursively##
+##        calls get_user_input() until user corretly validate file path or    ##
+##        user quits program; if user validates metadata file then calls      ##
+##        function output_file(counts_file, metadata_file); else if user does ##
+##        not validate the metadata_file output<-get_metadata() is recursively##
+##        called until file is validated or user exits program                ##
+################################################################################ 
+val_user_input <- function(counts_file, verification_1){
   if ((toupper(verification_1) == "YES") | (toupper(verification_1) == "Y")){
-    get_metadata <- function(){
-      cat("Full path to meta data file: ")
-      metadata_file <- trimws(readLines('stdin', n=1), which="both")
-      cat("You entered the following: ", metadata_file)
-      cat("\n")
-      cat("Is this correct? (Y/N/Q)")
-      verification_2 <- trimws(readLines('stdin', n=1), which="both")
-      return(list(metadata_file, verification_2))
-    }
     output <- get_metadata()
     if (toupper(unlist(output[2]))=="YES" | toupper(unlist(output[2]))=='Y'){
-      return(output_file(counts_file = count_file, metadata_file=unlist(output[1])))
+      return(output_file(counts_file = counts_file, metadata_file=unlist(output[1])))
     }else if ((toupper(unlist(output[2])) == "NO") | (toupper(unlist(output[2])) == 'N')){
-      return(output <- get_metadata())
+      output <- get_metadata()
+      return(val_user_input(counts_file = counts_file, verification_1 = verification_1))
     }else{
       stop("Exiting Program.  Goodbye!")
     }
+  }else if((toupper(verification_1) == "NO") | (toupper(verification_1) == 'N')){
+    return(get_user_input())
+  }else{
+    stop("Exiting Program.  Goodbye!")
   }
 }
 
 
 ################################################################################
+##function: get_metadata()                                                    ##
+##input:  None -- prompts user for location of metadata_file (properly        ## 
+##        formatted)                                                          ##
+##output: Takes user input and returns list with meta data file path and      ## 
+##        verifcation                                                         ##
+################################################################################ 
+get_metadata <- function(){
+  cat("Full path to meta data file: ")
+  metadata_file <- trimws(readLines('stdin', n=1), which="both")
+  cat("You entered the following: ", metadata_file)
+  cat("\n")
+  cat("Is this correct? (Y/N/Q)")
+  verification_2 <- trimws(readLines('stdin', n=1), which="both")
+  print(metadata_file)
+  print(verification_2)
+  return(list(metadata_file, verification_2))
+}
+
+
+
+################################################################################
 ##function: output_file()                                                     ##
 ##input: counts_file and metadata_file names and paths from function:         ##
-##       get_user_input()                                                     ##
+##       val_user_input(verification_1)                                       ##
 ##output: If output_file is verified then calls function:                     ## 
 ##        prep_data(counts_file, metadata_file, output_name); else if         ##
 ##        verification is equal to no, then recursively calls output_file()   ##
@@ -321,4 +350,54 @@ output_file <- function(counts_file, metadata_file){
 }
 
 get_user_input()
+
+
+
+
+
+##-----------------------------------------------------END OF WORKING SCRIPT--------------------------------------------------------##
+##-----------------------------------------EVERYTHING BELOW THIS LINE IS MANUAL VALIDATION------------------------------------------##
+
+##validation##
+validation_test <- function(){
+  pca_matrix <- read.csv("github_repositories/PCA_QC/pca_matrix_output.csv", header = T, row.names = 1)
+  metadata <- read.table("github_repositories/PCA_QC/metadata_sorted_output.csv", header=TRUE, row.names=1, check.names=FALSE, sep=",")
+  #categorical variables
+  linear_model <- lm(pca_matrix$PC1 ~ na.omit(as.factor(metadata$sex)))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  linear_model <- lm(pca_matrix$PC2 ~ na.omit(as.factor(metadata$sex)))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  linear_model <- lm(pca_matrix$PC3 ~ na.omit(as.factor(metadata$sex)))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  linear_model <- lm(pca_matrix$PC1 ~ na.omit(as.factor(metadata$diagnosis)))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  linear_model <- lm(pca_matrix$PC2 ~ na.omit(as.factor(metadata$diagnosis)))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  linear_model <- lm(pca_matrix$PC3 ~ na.omit(as.factor(metadata$diagnosis)))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  #numeric variables
+  linear_model <- lm(pca_matrix$PC1 ~ na.omit(metadata$contamination))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  linear_model <- lm(pca_matrix$PC2 ~ na.omit(metadata$contamination))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+  
+  linear_model <- lm(pca_matrix$PC3 ~ na.omit(metadata$contamination))
+  print(summary(linear_model)$adj.r.squared)
+  print(-log10(anova(linear_model)$Pr[1]))
+}
 
